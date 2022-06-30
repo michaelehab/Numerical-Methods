c1fd882338e913de446a4466d9025271b15c431e
Michael Ehab <michaelehab16@gmail.com>
2022-06-30 14:58:27 +0200

    Added Newton Divided Difference Interpolation method

--- interpolation.py
+++ interpolation.py
@@ -13,13 +13,24 @@ def lagrange(x, y, x_find):
 
     return y_ans
 
-def test_lagrange():
-    """ 
-    Example:
-        X = [1.0, 1.3, 1.5]
-        Y = [0.841, 0.964, 0.997]
-        P(1.4) = 0.9854
-    """
+def newton_divided_difference(x, y, x_find):
+    c = np.copy(y)
+
+    for j in range(1, x.size):
+        for i in range(x.size - 1, j - 1, -1):
+            c[i] = float(c[i] - c[i-1]) / float(x[i] - x[i - j])
+
+    y_ans = 0
+    for i in range(x.size):
+        f = c[i]
+        for j in range(i):
+            f *= (x_find - x[j])
+
+        y_ans += f
+        
+    return y_ans
+
+def input_data():
     x = np.array([])
     y = np.array([])
 
@@ -33,9 +44,36 @@ def test_lagrange():
 
     x_find = float(input("Find Y for X = "))
 
-    print("x", x)
-    print("y", y)
+    return [x, y, x_find]
+
+def test_lagrange():
+    """ 
+    Example:
+        X = [1.0, 1.3, 1.5]
+        Y = [0.841, 0.964, 0.997]
+        P(1.4) = 0.9854
+    """
+
+    x, y, x_find = input_data()
 
     y_ans = lagrange(x, y, x_find)
 
+    print("Using Lagrange Method :")
     print("F({}) = {}".format(x_find, y_ans))
+
+def test_newton():
+    """ 
+    Example:
+        X = [1.0, 1.3, 1.5]
+        Y = [0.841, 0.964, 0.997]
+        P(1.4) = 0.9854
+    """
+
+    x, y, x_find = input_data()
+
+    y_ans = newton_divided_difference(x, y, x_find)
+
+    print("Using Newton Divided Difference Method :")
+    print("F({}) = {}".format(x_find, y_ans))
+
+test_lagrange();
