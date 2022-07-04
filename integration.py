bf8bd407f7f6c110c167d8be1662a8abf9ac5b8d
Michael Ehab <michaelehab16@gmail.com>
2022-07-04 22:14:54 +0200

    Added Simpson 1/3 Rule for Integration

--- integration.py
+++ integration.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 def f1(x):
     return 1 / (1 + x)
@@ -11,6 +12,19 @@ def trapezoidal(start, end, n):
         ans += 2 * f1(start + h)
         start += h
     return ans * (h / 2)
+
+def simpson(start, end, n):
+    h = float((end - start) / n)
+    ans = f1(start) + f1(end)
+    i = 0
+    while(start + h != end):
+        if i % 2 == 0 : 
+            ans += 4 * f1(start + h)
+        else:
+            ans += 2 * f1(start + h)
+        start += h
+        i += 1
+    return ans * (h / 3)  
 
 def test_trapezoidal():
     """
@@ -26,4 +40,18 @@ def test_trapezoidal():
     ans = trapezoidal(start, end, n)
     print("The answer is {}".format(ans))
 
-test_trapezoidal()
+def test_simpson():
+    """
+    Example:
+        F(X) = e^(x) * sin(x)
+        Integration from 0 to 0.1 using 1 segment
+        = 0.09531
+    """
+    print("Integrating F(x) using Simpson rule:")
+    start = float(input("Enter the start point: "))
+    end = float(input("Enter the end point: "))
+    n = int(input("Enter the number of segments: "))
+    ans = simpson(start, end, n)
+    print("The answer is {}".format(ans))
+
+test_simpson()
