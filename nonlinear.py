7545b6e47f23e2825fbfc5cb23f8805c57c4b612
Michael Ehab <michaelehab16@gmail.com>
2022-07-06 11:45:56 +0200

    Added Gauss Seidel method in Non Linear

--- nonlinear.py
+++ nonlinear.py
@@ -19,11 +19,26 @@ def fixed_point(g1, g2, initial_guess, tolerance):
             return [v1, v2]
         prev_v1, prev_v2 = v1, v2
         v1, v2 = g1(prev_v1, prev_v2), g2(prev_v1, prev_v2)
-        
+
+def gauss_seidel(g1, g2, initial_guess, tolerance):
+    v1 = g1(initial_guess[0], initial_guess[1])
+    v2 = g2(initial_guess[0], initial_guess[1])
+    prev_v1, prev_v2 = initial_guess[0], initial_guess[1]
+    while True:
+        if(abs(v1 - prev_v1) < tolerance):
+            return [v1, v2]
+        prev_v1, prev_v2 = v1, v2
+        v1 = g1(prev_v1, prev_v2)
+        v2 = g2(v1, prev_v2)
 
 def test_fixed_point():
     initial_guess = [0, 0]
     x1, x2 = fixed_point(g1, g2, initial_guess, 1e-3)
     print("X1 = {}, X2 = {}".format(x1, x2))
 
-test_fixed_point()
+def test_gauss_seidel():
+    initial_guess = [0, 0]
+    x1, x2 = gauss_seidel(g1, g2, initial_guess, 1e-3)
+    print("X1 = {}, X2 = {}".format(x1, x2))
+
+test_gauss_seidel()
