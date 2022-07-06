6404f8d91fe523af6c86e7f000efad2bc7f18d78
Michael Ehab <michaelehab16@gmail.com>
2022-07-06 12:07:57 +0200

    Added Newton method in Non linear

--- nonlinear.py
+++ nonlinear.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def f1(x1, x2):
     return x1**2 - 10*x1 + x2**2 + 8
 
@@ -9,6 +11,24 @@ def g1(x1, x2):
     
 def g2(x1, x2):
     return (x1 * x2**2 + x1 + 8) / 10
+
+def f3(x1, x2):
+    return 4 * x1**2 - 20 * x1 + 0.25 * x2**2 + 8
+
+def pf3px1(x1, x2):
+    return 8 * x1 - 20
+
+def pf3px2(x1, x2):
+    return 0.5 * x2
+
+def f4(x1, x2):
+    return 0.5 * x1 * x2**2 + 2 * x1 - 5 * x2 + 8
+
+def pf4px1(x1, x2):
+    return 0.5 * x2 **2 + 2
+
+def pf4px2(x1, x2):
+    return x1 * x2 - 5
 
 def fixed_point(g1, g2, initial_guess, tolerance):
     v1 = g1(initial_guess[0], initial_guess[1])
@@ -31,6 +51,19 @@ def gauss_seidel(g1, g2, initial_guess, tolerance):
         v1 = g1(prev_v1, prev_v2)
         v2 = g2(v1, prev_v2)
 
+def J(pf1px1, pf2px1, pf1px2, pf2px2, x1, x2):
+    return np.array([[pf1px1(x1, x2), pf1px2(x1, x2)], [pf2px1(x1, x2), pf2px2(x1, x2)]])
+
+def newton(pf1px1, pf2px1, pf1px2, pf2px2, f1, f2, x1, x2, steps):
+    for i in range(steps):
+        y = np.matmul(np.array([-f1(x1, x2), -f2(x1, x2)]), np.linalg.inv(J(pf1px1, pf2px1, pf1px2, pf2px2, x1, x2)))
+        x1 += y[0]
+        x2 += y[1]
+        print("Iteration {} : X1 = {}, X2 = {}".format(i + 1, x1, x2))
+
+    return [x1, x2]
+    
+
 def test_fixed_point():
     initial_guess = [0, 0]
     x1, x2 = fixed_point(g1, g2, initial_guess, 1e-3)
@@ -41,4 +74,9 @@ def test_gauss_seidel():
     x1, x2 = gauss_seidel(g1, g2, initial_guess, 1e-3)
     print("X1 = {}, X2 = {}".format(x1, x2))
 
-test_gauss_seidel()
+def test_newton():
+    x1, x2 = newton(pf3px1, pf4px1, pf3px2, pf4px2, f3, f4, 0, 0, 3)
+    print("F1(X1, X2) = {}".format(f3(x1, x2)))
+    print("F2(X1, X2) = {}".format(f4(x1, x2)))
+
+test_newton()
